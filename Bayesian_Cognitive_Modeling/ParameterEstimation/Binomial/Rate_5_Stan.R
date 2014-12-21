6b5c674db98c9b671f943f2e6d37941507571cc1
neekro <smira.martin@gmail.com>
2014-12-21 17:57:25 +0100

    fix plot in Rate_5

--- Bayesian_Cognitive_Modeling/ParameterEstimation/Binomial/Rate_5_Stan.R
+++ Bayesian_Cognitive_Modeling/ParameterEstimation/Binomial/Rate_5_Stan.R
@@ -5,13 +5,13 @@ library(rstan)
 
 # to be passed on to Stan
 data <- read_rdump("Rate_5.data.R")
- 
+
 myinits <- list(
   list(theta=.5))
 
 # parameters to be monitored:  
 parameters <- c("theta", "postpredk1", "postpredk2")
-      
+
 # The following command calls Stan with specific options.
 # For a detailed description type "?stan".
 samples <- stan(file="Rate_5_model.stan",   
@@ -29,7 +29,7 @@ print(samples)
 theta      <- extract(samples)$theta
 postpredk1 <- extract(samples)$postpredk1
 postpredk2 <- extract(samples)$postpredk2
-      
+
 # Two-panel plot. 
 layout(matrix(c(1,2),1,2))
 layout.show(2)
@@ -44,13 +44,13 @@ plot(c(y$breaks, max(y$breaks)), c(0,y$density,0), type="S", lwd=2, lty=1,
 lines(density(theta), col="red", lwd=2)
 
 # Second plot, the data space (predictives)
-plot(k1,k2,type="p", pch=4, cex=2, lwd=2, xlab="Success Count 1",
-     ylab="Success Count 2", xlim=c(-1, n1+1), ylim=c(-1,n2+1))        
+plot(data$k1, data$k2, type="p", pch=4, cex=2, lwd=2, xlab="Success Count 1",
+     ylab="Success Count 2", xlim=c(-1, data$n1+1), ylim=c(-1, data$n2+1))        
 nsamples <- length(theta)
 sc <- 10
-for (i in 0:n1)
+for (i in 0:data$n1)
 {
-  for (j in 0:n2) 
+  for (j in 0:data$n2) 
   {
     match.preds <- sum(postpredk1==i & postpredk2==j)/nsamples
     if (match.preds > 0)
