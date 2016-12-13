88b36ea0dc412bdc9545db881fa2f9d2e82d0618
neekro <smira.martin@gmail.com>
2016-12-14 00:29:09 +0100

    change R package

--- Bayesian_Cognitive_Modeling/ModelSelection/Means/OneSample_Stan.R
+++ Bayesian_Cognitive_Modeling/ModelSelection/Means/OneSample_Stan.R
@@ -1,24 +1,24 @@
-# clears workspace: 
-rm(list=ls()) 
+# clears workspace:
+rm(list=ls())
 
 library(rstan)
 
 model <- "
 // One-Sample Comparsion of Means
-data { 
+data {
   int<lower=0> ndata;
   vector[ndata] x;
 }
 parameters {
   real sigmatmp;
   real delta;
-} 
+}
 transformed parameters {
   real mu;
   real<lower=0> sigma;
 
-  sigma <- fabs(sigmatmp);
-  mu <- delta * sigma;
+  sigma = fabs(sigmatmp);
+  mu = delta * sigma;
 }
 model {
   // Delta and sigma Come From (Half) Cauchy Distributions
@@ -56,26 +56,26 @@ parameters <- c("delta")
 
 # The following command calls Stan with specific options.
 # For a detailed description type "?rstan".
-samples <- stan(model_code=model,   
-                data=data, 
+samples <- stan(model_code=model,
+                data=data,
                 init=myinits,  # If not specified, gives random inits
                 pars=parameters,
-                iter=20000, 
-                chains=3, 
-                thin=1,
+                iter=20000,
+                chains=3,
+                thin=1
                 # warmup=100,  # Stands for burn-in; Default = iter/2
                 # seed=123  # Setting seed; Default is random seed
 )
-# Now the values for the monitored parameters are in the "samples" object, 
+# Now the values for the monitored parameters are in the "samples" object,
 # ready for inspection.
 
 plot(samples)
 
 # Collect posterior samples across all chains:
-delta.posterior <- extract(samples)$delta  
+delta.posterior <- extract(samples)$delta
 
 #============ BFs based on logspline fit ===========================
-library(polspline) # this package can be installed from within R
+library(logspline) # this package can be installed from within R
 fit.posterior <- logspline(delta.posterior)
 
 # 95% confidence interval:
@@ -96,7 +96,7 @@ yhigh <- 4
 Nbreaks <- 80
 y       <- hist(delta.posterior, Nbreaks, plot=F)
 plot(c(y$breaks, max(y$breaks)), c(0,y$density,0), type="S", lwd=2, lty=2,
-     xlim=c(xlow,xhigh), ylim=c(0,yhigh), xlab=" ", ylab="Density", axes=F) 
+     xlim=c(xlow,xhigh), ylim=c(0,yhigh), xlab=" ", ylab="Density", axes=F)
 axis(1, at = c(-4,-3,-2,-1,0,1,2,3,4), lab=c("-4","-3","-2","-1","0",
                                              "1", "2", "3", "4"))
 axis(2)
@@ -108,7 +108,7 @@ points(0, dlogspline(0, fit.posterior),pch=19, cex=2)
 # plot the prior:
 par(new=T)
 plot(function( x ) dcauchy( x, 0, 1 ), xlow, xhigh, ylim=c(0,yhigh),
-     xlim=c(xlow,xhigh), lwd=2, lty=1, ylab=" ", xlab = " ", axes=F) 
+     xlim=c(xlow,xhigh), lwd=2, lty=1, ylab=" ", xlab = " ", axes=F)
 axis(1, at = c(-4,-3,-2,-1,0,1,2,3,4), lab=c("-4","-3","-2","-1","0",
                                              "1", "2", "3", "4"))
 axis(2)
