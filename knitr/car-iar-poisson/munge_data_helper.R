3d68b7d560a68a57fa0b71e75ecf765e54046cb5
Mitzi Morris <mitzi@panix.com>
2017-06-08 14:54:10 -0400

    version 2:  focus on BYM models, add Scotland Lip Cancer example

--- knitr/car-iar-poisson/munge_data_helper.R
+++ knitr/car-iar-poisson/munge_data_helper.R
@@ -1,8 +1,8 @@
 get_nb_off_diags = function(x) {
      n_tracts = length(x);
      n_links = sum(card(x));
-     idx_i = vector(mode="numeric",length=n_links);
-     idx_j = vector(mode="numeric",length=n_links);
+     idx_i = vector(mode="numeric", length=n_links);
+     idx_j = vector(mode="numeric", length=n_links);
      idx = 1;
      for (i in 1:n_tracts) {
         if (x[[i]][1] != 0) {
@@ -13,10 +13,8 @@ get_nb_off_diags = function(x) {
            }
         }
      }
-     return(cbind(idx_i,idx_j));
+     t1 =  cbind(idx_i, idx_j);
+     t2 = aggregate(count~idx_i+idx_j, transform(t1, idx_i=pmin(idx_i, idx_j), idx_j=pmax(idx_i, idx_j), count=1), sum)
+     t3 = data.frame(t2$idx_i, t2$idx_j);
+     return (as.matrix(t3));
 }
-
-
-  
-  
-  
