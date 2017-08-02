144a231b5d0887bfa6bd77ffe9af75f995fabb79
Rob Trangucci <robert.trangucci@gmail.com>
2017-08-02 13:13:30 -0400

    Final datasets

--- misc/gaussian-process/gp-sim.R
+++ misc/gaussian-process/gp-sim.R
@@ -1,16 +1,15 @@
 library("rstan")
 library("ggplot2")
 
+set.seed(123)
+
 x <- (-50:50)/25
 N <- length(x)
 
 comp_sim <- stan_model(file="gp-sim.stan")
-fit_sim <- sampling(comp_sim, data = list(x = x, N = N), iter = 300, chains = 3, seed = 123)
+fit_sim <- sampling(comp_sim, data = list(x = x, N = N), iter = 100, chains = 3, seed = 123)
 
 fit_sim_ss <- extract(fit_sim)
-
-df <- data.frame(x=x,y_sim=colMeans(fit_sim_ss$y))
-plot <- qplot(x,y_sim, data=df, xlim=c(-2,2),geom = 'line')
 
 # DUMP DATA: gp-sim.stan
 dump(c("N","x"),file="gp-sim.data.R")
@@ -32,7 +31,6 @@ dump(c("N1","x1","y1","N2","x2"),file="gp-predict.data.R")
 # DUMP DATA: gp-logit-predict.stan
 y1 <- fit_sim_ss$y[1,] # any sample will do
 z1 <- rep(0,length(y1))
-set.seed(123)
 z1 <- rbinom(N1, 1,plogis(y1))
 dump(c("N1","x1","z1","N2","x2"),file="gp-logit-predict.data.R")
 
