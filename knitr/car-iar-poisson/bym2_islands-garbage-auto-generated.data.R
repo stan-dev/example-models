9917f716b90cc072ae75bb2fd9dc020cc6dd5d4c
Sean Talts <sean.talts@gmail.com>
2019-06-27 13:02:13 -0400

    Add some garbage generated data for testing

---
+++ knitr/car-iar-poisson/bym2_islands-garbage-auto-generated.data.R
@@ -0,0 +1,10 @@
+N <- 2
+N_edges <- 3
+node1 <- c(2, 2, 1)
+node2 <- c(1, 1, 2)
+y <- c(3, 2)
+E <- c(2.4293360748575767, 1.205148245631666)
+N_singletons <- 1
+N_components <- 2
+nodes_per_component <- c(1, 1)
+scales <- c(6.6603721061434964, 2.6106403377361884)
