f25f280e29027b2987ebe0ffcc3d9ddea052ae17
Mitzi Morris <mitzi@panix.com>
2023-09-09 13:54:53 -0400

    update to CmdStanR

--- knitr/car-iar-poisson/fit_scotland.R
+++ knitr/car-iar-poisson/fit_scotland.R
@@ -1,5 +1,8 @@
-library(rstan)   
-options(mc.cores = parallel::detectCores())  
+library(devtools)
+if(!require(cmdstanr)){
+  devtools::install_github("stan-dev/cmdstanr", dependencies=c("Depends", "Imports"))
+}
+library(cmdstanr)   
 
 source("mungeCARdata4stan.R")  
 source("scotland_data.R")
@@ -13,11 +16,22 @@ node1 = nbs$node1;
 node2 = nbs$node2;
 N_edges = nbs$N_edges;
 
-scot_stanfit = stan("bym_predictor_plus_offset.stan",
-         data=list(N,N_edges,node1,node2,y,x,E),
-         iter=10000);
+data = list(N=N,N_edges=N_edges,node1=node1,node2=node2,y=y,x=x,E=E)
 
-print(scot_stanfit,
-      pars=c("lp__", "beta0", "beta1", "sigma_phi", "tau_phi", "sigma_theta", "tau_theta","mu[5]","phi[5]","theta[5]"),
-      probs=c(0.025, 0.5, 0.975),digits=3);
+bym_model = cmdstan_model("bym_predictor_plus_offset.stan");
+scot_stanfit = bym_model$sample(
+         data = data,
+         parallel_chains = 4,
+         iter_warmup = 5000,
+         iter_sampling = 5000);
+
+options(digits=3)
+scot_stanfit$summary(variables = c("lp__", "beta0", "beta1",
+                                   "sigma_phi", "tau_phi",
+                                   "sigma_theta", "tau_theta",
+                                   "mu[5]","phi[5]","theta[5]"),
+                     ~quantile(.x, probs = c(0.025, 0.5, 0.975)))
+
+
+
 
