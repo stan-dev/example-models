c5d06419f1cf228845237d3524cbd16bc7c64ff0
Gilles Lijnzaad <gilleslijnzaad@gmail.com>
2025-06-26 16:38:39 +0200

    Stan code is now in separate files; updated some deprecated Stan functions

--- Bayesian_Cognitive_Modeling/ParameterEstimation/DataAnalysis/Planes_Stan.R
+++ Bayesian_Cognitive_Modeling/ParameterEstimation/DataAnalysis/Planes_Stan.R
@@ -3,45 +3,6 @@ rm(list=ls())
 
 library(rstan)
 
-#### Notes to Stan model #######################################################
-## Stan has hypergeometric distribution implemented, so in one way the code
-## is more intuitive. On the other hand, Stan can't sample discrete parameters,
-## therefore we have to increment log probability manually (as we did in 
-## Survey example).  
-################################################################################
-model <- "
-// Planes
-data { 
-  int<lower=1> x;
-  int<lower=1> n;
-  int<lower=0,upper=n> k;
-  int<lower=x> tmax;
-}
-transformed data {
-  int<lower=x> tmin;
-    
-  tmin <- x + n - k;
-}
-transformed parameters {
-  vector[tmax] lp_parts;
-
-  for (t in 1:tmax) 
-    if (t < tmin)
-      lp_parts[t] <- log(1.0 / tmax) + negative_infinity();  // Zero probability
-    else
-      lp_parts[t] <- log(1.0 / tmax) + hypergeometric_log(k, n, x, t - x);
-}
-model {
-  increment_log_prob(log_sum_exp(lp_parts));
-}
-generated quantities {
-  int<lower=tmin,upper=tmax> t;
-  simplex[tmax] tp;
-
-  tp <- softmax(lp_parts);
-  t <- categorical_rng(tp);
-}"
-
 x <- 10  # number of captures
 k <- 4  # number of recaptures from n
 n <- 5  # size of second sample
